7aa698e45c320a3a22704781a040e86be6e1e690
traveler-leon <liyong13994@163.com>
2024-01-21 23:31:09 +0800

    langchain-core 的prompt的uml梳理

---
+++ demo-1.py
@@ -0,0 +1,7 @@
+from langchain.prompts import PromptTemplate
+
+prompt_template = PromptTemplate.from_template(
+    "Tell me a {adjective} joke about {content}"
+)
+res = prompt_template.format(adjective="funny",content="chickens")
+print(res)
