01cc205b1dedf256440fd720727a53e7e9cddc25
traveler-leon <liyong13994@163.com>
2024-01-22 17:58:01 +0800

    添加promptvalue

--- demo-1.py
+++ demo-1.py
@@ -1,9 +1,16 @@
 from langchain.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
 prompt_template = PromptTemplate.from_template(
     "Tell me a {adjective} joke about {content}"
 )
 res = prompt_template.format(adjective="funny",content="chickens")
-print(res)
-
-print("test")
+res2 = prompt_template.format_prompt(adjective="funny",content="chickens")
+print(res,res2)
+print(type(res),type(res2))
+print(prompt_template.InputType,prompt_template.OutputType)
+print(prompt_template.input_schema,prompt_template.output_schema)
+print(prompt_template.get_input_schema())
+print(prompt_template.get_graph())
+print(prompt_template.get_prompts())
